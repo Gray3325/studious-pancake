74b8ea3e52c3c8ccbeb06408a6846980fb4533a9
Gray <blackcat5523@gmail.com>
2024-01-29 16:16:35 +0800

    用箭頭函式更改每個點擊後的狀態

--- fw/src/App.js
+++ fw/src/App.js
@@ -15,23 +15,25 @@ export default function Board() {
     nextSquares[i] = "X";
     setSquares(nextSquares);
   }
-  // 使用useState來記錄每一格的狀態
+  /*  使用useState來記錄每一格的狀態
+      用箭頭函式更改每個點擊後的狀態
+  */
   return (
     <>
       <div className='board-row'>
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square value={squares[1]} onSquareClick={handleClick} />
-        <Square value={squares[2]} onSquareClick={handleClick} />
+        <Square value={squares[0]} onSquareClick={()=> handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={()=> handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={()=> handleClick(2)} />
       </div>
       <div className='board-row'>
-        <Square value={squares[3]} onSquareClick={handleClick} />
-        <Square value={squares[4]} onSquareClick={handleClick} />
-        <Square value={squares[5]} onSquareClick={handleClick} />
+        <Square value={squares[3]} onSquareClick={()=> handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={()=> handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={()=> handleClick(5)} />
       </div>
       <div className='board-row'>
-        <Square value={squares[6]} onSquareClick={handleClick} />
-        <Square value={squares[7]} onSquareClick={handleClick} />
-        <Square value={squares[8]} onSquareClick={handleClick} />
+        <Square value={squares[6]} onSquareClick={()=> handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={()=> handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={()=> handleClick(8)} />
       </div>
     </>
   );
