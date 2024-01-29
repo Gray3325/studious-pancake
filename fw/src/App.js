fc2981b8f4d47e61ce178705204338343e9c1fa8
Gray <blackcat5523@gmail.com>
2024-01-29 15:54:34 +0800

    狀態更新

--- fw/src/App.js
+++ fw/src/App.js
@@ -1,34 +1,30 @@
 import { useState } from "react"; //導入useState狀態組件
-function Square() {
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    setValue("X");
-  }
+function Square() {  //把原本會帶入Ｘ的狀態刪掉
   return (
-    <button className='square' onClick={handleClick}>
+    <button className='square'>
       {value}
     </button>
   );
 }
 
 export default function Board() {
-  
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]}/>
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square value='6' />
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
       </div>
     </>
   );
